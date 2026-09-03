6055e928052da52c90b2a66484049f8a34785f5b
madebysag <abdulgafarsulaimon3@gmail.com>
2026-09-03 15:38:57 +0100

    Demos Display pages content nowchanges arccordingly

--- playground/script.js
+++ playground/script.js
@@ -3,15 +3,22 @@ const displayPage = document.querySelector(".demos_display")
 const demosLinks = document.querySelectorAll("main a.demos_link")
 
 const backToMenuBtn = displayPage.querySelector(".page_nav button")
+const displayTitle = displayPage.querySelector("header h4.text-black")
+const displayArticle = displayPage.querySelector("section.article")
+
+const fullscreenBtn = displayPage.querySelector(".fullscreen_toggle")
 const loadingSpinner = displayPage.querySelector(".loading")
 const iFrame = displayPage.querySelector("iframe")
 
-function showDemo(url) {
+function showDemo(link) {
     landingPage.hidden = true;
     displayPage.hidden = false;
     loadingSpinner.hidden = false;
+
+    displayTitle.innerHTML = link ? link.innerText : "";
+    displayArticle.innerHTML = (link && link.children[0]) ? link.children[0].innerHTML : "";
     
-    iFrame.src = url;
+    iFrame.src = link.href;
     iFrame.onload = () => {
         loadingSpinner.hidden = true;
     };
@@ -22,14 +29,22 @@ function hideDemo() {
     displayPage.hidden = true;
 }
 
+function toggleFullscreen() {
+    
+}
+
 demosLinks.forEach(link => {
     link.addEventListener("click", e => {
         e.preventDefault()
 
-        showDemo(e.target.href)
+        showDemo(e.currentTarget)
     })
 })
 
 backToMenuBtn.addEventListener("click", e => {
     hideDemo()
+})
+
+fullscreenBtn.addEventListener("click", e => {
+    toggleFullscreen()
 })
